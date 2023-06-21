8710600404ddcddf29ffd674238abdf78b139317
bskaar <bruce.skaar@gmail.com>
2023-06-20 19:22:18 -0700

    Updated JS and READ.ME

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # leaflet-challenge
 Module 15 Challenge
+
+Acknowledgements: Ahmad Sweed going through Activity 15 on Day 3 Citibike Leaflet that helped in figuring out how to complete the Module 15 Challenge.
+
+The HTML link can be found in the repository and http here: https://bskaar.github.io/leaflet-challenge/
+
+In the Repository, I have saved several PNG of the following:
+
+1. Leaflet map coordinates rendered from the data: Leaflet_map_coordinates rendered correctly.png
+
+2. Completed Leaflet map with earthquake coordinates represented as a bubble: Completed Leaflet map showing earthquake activity.png
+
+3. Zoomed in view of a specific earthquake point: Zoom in of earthquake data point.png
+
--- static/js/logic.js
+++ static/js/logic.js
@@ -15,38 +15,66 @@ script.onload = function () {
   fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson')
     .then(response => response.json())
     .then(data => {
+      // Define function to calculate marker size based on magnitude
+      function getMarkerSize(magnitude) {
+        return Math.sqrt(magnitude) * 4;
+      }
+
+      // Define function to calculate marker color based on depth
+      function getMarkerColor(depth) {
+        if (depth < 10) {
+          return '#7fff00'; // Shallow depth (green)
+        } else if (depth < 50) {
+          return '#ffd700'; // Intermediate depth (gold)
+        } else {
+          return '#ff4500'; // Deep depth (orange-red)
+        }
+      }
+
       // Process the GeoJSON data and add markers for each earthquake
       data.features.forEach(feature => {
         var magnitude = feature.properties.mag;
         var place = feature.properties.place;
-        var coordinates = feature.geometry.coordinates;
+        var depth = feature.geometry.coordinates[2];
+        var coordinates = [feature.geometry.coordinates[1], feature.geometry.coordinates[0]];
 
-        // Create a Leaflet marker with a popup for each earthquake
-        L.marker([coordinates[1], coordinates[0]])
-          .bindPopup(`<strong>Magnitude: ${magnitude}</strong><br>Location: ${place}`)
+        // Create a Leaflet circle marker with size and color based on magnitude and depth
+        L.circleMarker(coordinates, {
+          radius: getMarkerSize(magnitude),
+          fillColor: getMarkerColor(depth),
+          color: '#000',
+          weight: 1,
+          opacity: 1,
+          fillOpacity: 0.8
+        })
+          .bindPopup(`<strong>Magnitude: ${magnitude}</strong><br>Location: ${place}<br>Depth: ${depth} km`)
           .addTo(map);
       });
+
+      // Create a legend
+      var legend = L.control({ position: 'bottomright' });
+      legend.onAdd = function () {
+        var div = L.DomUtil.create('div', 'legend');
+        var depths = [0, 10, 50];
+        var colors = ['#7fff00', '#ffd700', '#ff4500'];
+        var labels = ['Shallow', 'Intermediate', 'Deep'];
+
+        // Add legend title
+        div.innerHTML = '<strong>Depth Legend</strong><br>';
+
+        // Add legend items
+        for (var i = 0; i < depths.length; i++) {
+          div.innerHTML += `<i style="background: ${colors[i]}"></i> ${depths[i]} - ${labels[i]}<br>`;
+        }
+
+        return div;
+      };
+      legend.addTo(map);
     })
     .catch(error => {
       console.error('Error:', error);
     });
 };
 document.head.appendChild(script);
-
-// CSS styles
-var styles = `
-  #map {
-    height: 500px;
-  }
-`;
-var styleNode = document.createElement('style');
-styleNode.innerHTML = styles;
-document.head.appendChild(styleNode);
-
-// HTML container
-var container = `
-  <div id="map"></div>
-`;
-document.body.innerHTML = container;
 
   
